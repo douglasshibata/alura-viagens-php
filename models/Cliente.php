286e265f7468f8a8dfb781773406a884c625ee4c
Douglas Shibata <douglas.shibata11@gmail.com>
2022-07-12 17:22:41 -0300

    Formatando e validando email

--- models/Cliente.php
+++ models/Cliente.php
@@ -33,6 +33,10 @@ class Cliente
         if (!$this->telefoneValido($telefone)) {
             throw new Exception("Telefone no formato inválido");
         }
+
+        if (!$this->emailValido($email)) {
+            throw new Exception("E-mail no formato inválido");
+        }
         $this->nome = $nome;
         $this->cpf_cnpj = $cpf_cnpj;
         $this->telefone = $telefone;
@@ -59,6 +63,13 @@ class Cliente
         if (strlen($telefone) == 15) {
             $regex_telefone = "/\([0-9]{2}\)[0-9]{5}\-[0-9]{4}/";
             return preg_match($regex_telefone, str_replace(" ", "", $telefone));
+        }
+        return false;
+    }
+    function emailValido($email)
+    {
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return true;
         }
         return false;
     }
