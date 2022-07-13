a587b3599bdf9bf0e73ca017f71d664bc16f3281
Douglas Shibata <douglas.shibata11@gmail.com>
2022-07-13 15:01:13 -0300

    Adicionando formatador para salvar no banco sem e exibir para o usuário com

--- models/Cliente.php
+++ models/Cliente.php
@@ -1,6 +1,6 @@
 <?php
-require_once("ValidadorCPF.php");
-require_once("ValidadorCPNJ.php");
+require_once "ValidadorCPF.php";
+require_once "ValidadorCNPJ.php";
 class Cliente
 {
 
@@ -27,31 +27,26 @@ class Cliente
         $cidade,
         $uf
     ) {
+
         $validadorCPF = new ValidadorCPF();
         $validadorCNPJ = new ValidadorCNPJ();
+
+        //validação
+        if (!$this->cepValido($cep)) throw new Exception("CEP no formato inválido");
+        if (!$this->telefoneValido($telefone)) throw new Exception("Telefone no formato inválido");
+        if (!$this->emailValido($email)) throw new Exception("Email no formato inválido");
 
         if (strlen($cpf_cnpj) > 14) {
             if (!$validadorCNPJ->ehValido($cpf_cnpj)) throw new Exception("CNPJ inválido");
         } else {
-            if (!$validadorCPF->ehValido($cpf_cnpj)) {
-                throw new Exception("CPF inválido");
-            }
-        }
-        if (!$this->cepValido($cep)) {
-            throw new Exception("CEP no formato inválido");
-        }
-        if (!$this->telefoneValido($telefone)) {
-            throw new Exception("Telefone no formato inválido");
+            if (!$validadorCPF->ehValido($cpf_cnpj)) throw new Exception("CPF Inválido");
         }
 
-        if (!$this->emailValido($email)) {
-            throw new Exception("E-mail no formato inválido");
-        }
         $this->nome = $nome;
-        $this->cpf_cnpj = $cpf_cnpj;
-        $this->telefone = $telefone;
+        $this->cpf_cnpj = $this->removeFormatacao($cpf_cnpj);
+        $this->telefone = $this->removeFormatacao($telefone);
         $this->email = $email;
-        $this->cep = $cep;
+        $this->cep = $this->removeFormatacao($cep);
         $this->endereco = $endereco;
         $this->bairro = $bairro;
         $this->numero = $numero;
@@ -62,25 +57,37 @@ class Cliente
     function cepValido($cep)
     {
         if (strlen($cep) == 10) {
+            //22.333-333
             $regex_cep = "/[0-9]{2}\.[0-9]{3}\-[0-9]{3}/";
             return preg_match($regex_cep, $cep);
         } else {
             return false;
         }
     }
+
     function telefoneValido($telefone)
     {
         if (strlen($telefone) == 15) {
+            //(99) 99999-9999
             $regex_telefone = "/\([0-9]{2}\)[0-9]{5}\-[0-9]{4}/";
             return preg_match($regex_telefone, str_replace(" ", "", $telefone));
+        } else {
+            return false;
         }
-        return false;
     }
+
     function emailValido($email)
     {
         if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
             return true;
+        } else {
+            return false;
         }
-        return false;
+    }
+
+    function removeFormatacao($info)
+    {
+        $dado = str_replace([".", "-", "/", "(", ")", " "], "", $info);
+        return $dado;
     }
 }
